553386f580ebc9be9de4e0c7c5b94ce3d3caf9e6
sinar96 <sinar96>
2020-01-15 20:37:36 +0800

    Learn about Data Type

---
+++ tugas2.js
@@ -0,0 +1,19 @@
+var bulan = "30";
+
+var string = "Hallo koding";
+
+var boolean = true;
+
+var kosong = null;
+
+var tdk_terdefinisi;
+
+var array = [23, 24, 25, 26]
+
+console.log(bulan)
+console.log(string)
+console.log(boolean)
+console.log(kosong)
+console.log(tdk_terdefinisi)
+console.log(array)
+console.log(array [2])
